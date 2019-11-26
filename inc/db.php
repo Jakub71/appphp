c2dffefc20380a5a0b20191d7bd3935e0fd08ce8
Jakub71 <Jakubjan0208@interia.pl>
2019-11-26 09:30:24 +0100

    komit1

--- inc/db.php
+++ inc/db.php
@@ -6,43 +6,48 @@ class Baza {
     var $mode = PDO::FETCH_ASSOC;
     var $kom = array();
     
-    function __construct(){
-        global $db, $dbfile, $kom;
+    function __construct($dbfile){
         if (!file_exists($dbfile))
-            $kom[]= "Brak pliku bazy tworze nowy.";
+            $this->kom[]= "Brak pliku bazy tworze nowy.";
+        try {
+         $this->db = new PDO("sqlite:$dbfile");
+         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+         $this->kom[]='Błąd'.$e->getMessage()."\n";
+        }
+        $this->init_tables();
         
-        $db = new PDO("sqlite:$dbfile");
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
     function init_tables() {
         global $db, $kom;
         if (file_exists('baza/baza.sql')){
             $q ="SELECT name FROM sqlite_master WHERE type='table' AND name='menu'";
-            $ret = array();
-            db_query($q, $ret);
-            if(empty($ret)){
+            $this->db_query($q);
+            if(empty($this->$ret)){
                 $sql = file_get_contents('baza/baza.sql');
-                $db->exec($sql);
-                $kom[]="Utworzono tabeleczkę";
+                $this->db_exec($sql);
+                $this->kom[]="Utworzono tabeleczkę";
             }
       }
     }
 
-    function db_query($q, &$ret){
-        global $db;
-        $r = null;
+    function db_query($q){
         try {
-            $r = $db->query($q);
+            $this->ret = $this->db->query($q, $this->mode)->fetchAll();
         } catch(PDOException $e){
-            echo ($e->getMessage());
+          $this->kom[]='Błąd'.$e->getMessage()."\n";
         }
-        if ($r){
-            $ret = $r->fetchAll();
-            return true;
-        }
-        return false;
+        
     }
 }
-
+function db_exec($q){
+        try {
+            $this->db->exec($q);
+        } catch(PDOException $e){
+          $this->kom[]='Błąd'.$e->getMessage()."\n";
+        }
+        
+    }
+}
 ?>
